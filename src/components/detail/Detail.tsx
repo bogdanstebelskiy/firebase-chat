7ff8492e2d3ade123f0c7aac40f9a0e7c586d2f8
bogdanstebelskiy <bogdanstebelskiy@gmail.com>
2024-06-03 18:19:11 +0300

    (feat): display chat images

--- src/components/detail/Detail.tsx
+++ src/components/detail/Detail.tsx
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import "./detail.css"
 import { auth, db } from '../../lib/firebase'
 import { useUserStore } from '../../lib/userStore';
 import { useChatStore } from '../../lib/chatStore';
-import { arrayRemove, arrayUnion, doc, updateDoc } from 'firebase/firestore';
+import { arrayRemove, arrayUnion, doc, onSnapshot, updateDoc } from 'firebase/firestore';
 
 const Detail = () => {
   // TODO: specify any type
+  const [chatImages, setChatImages]: any = useState([]);
   const { user, isCurrentUserBlocked, isReceiverBlocked, toggleBlock }: any = useChatStore();
   const { currentUser }: any = useUserStore();
+  const { chatId }: any = useChatStore();
+
+  useEffect(() => {
+    const unSub = onSnapshot(doc(db, "chats", chatId), async (res: any) => {
+      const items = res.data().messages;
+
+      const images: any = [];
+
+      items.forEach((message: any) => {
+        if (message.img) {
+          images.push(message?.img);
+        }
+      })
+      setChatImages(images);
+    })
+
+    return () => {
+      unSub();
+    }
+  }, [])
 
   const handleBlock = async () => {
     if (!user) {
@@ -55,34 +76,15 @@ const Detail = () => {
             <img src="./arrowDown.png" alt="" />
           </div>
           <div className="photos">
-            <div className="photoItem">
-              <div className="photoDetail">
-                <img src="https://i.redd.it/couldnt-find-a-good-hd-version-of-badmotorfinger-to-use-as-v0-cutf0bwk9mu91.png?width=1701&format=png&auto=webp&s=352a817f8ade9cf17cadd6f23e09afcd397fc48d" />
-                <span>photo_2024_2.png</span>
+            {chatImages.map((image: any) => (
+              <div className="photoItem">
+                <div className="photoDetail">
+                  <img src={`${image}`} />
+                  <span>photo_2024_2.png</span>
+                </div>
+                <img src="./download.png" alt="" className="icon" />
               </div>
-              <img src="./download.png" alt="" className="icon" />
-            </div>
-            <div className="photoItem">
-              <div className="photoDetail">
-                <img src="https://i.redd.it/couldnt-find-a-good-hd-version-of-badmotorfinger-to-use-as-v0-cutf0bwk9mu91.png?width=1701&format=png&auto=webp&s=352a817f8ade9cf17cadd6f23e09afcd397fc48d" />
-                <span>photo_2024_2.png</span>
-              </div>
-              <img src="./download.png" alt="" className="icon" />
-            </div>
-            <div className="photoItem">
-              <div className="photoDetail">
-                <img src="https://i.redd.it/couldnt-find-a-good-hd-version-of-badmotorfinger-to-use-as-v0-cutf0bwk9mu91.png?width=1701&format=png&auto=webp&s=352a817f8ade9cf17cadd6f23e09afcd397fc48d" />
-                <span>photo_2024_2.png</span>
-              </div>
-              <img src="./download.png" alt="" className="icon" />
-            </div>
-            <div className="photoItem">
-              <div className="photoDetail">
-                <img src="https://i.redd.it/couldnt-find-a-good-hd-version-of-badmotorfinger-to-use-as-v0-cutf0bwk9mu91.png?width=1701&format=png&auto=webp&s=352a817f8ade9cf17cadd6f23e09afcd397fc48d" />
-                <span>photo_2024_2.png</span>
-              </div>
-              <img src="./download.png" alt="" className="icon" />
-            </div>
+            ))}
           </div>
         </div>
         <div className="option">
@@ -91,13 +93,13 @@ const Detail = () => {
             <img src="./arrowUp.png" alt="" />
           </div>
         </div>
-      </div>
-      <div className="buttons">
-        <button className="block" onClick={handleBlock}>
-          {/* TODO: Block messages doesn't always show up right! Fix!*/}
-          {isCurrentUserBlocked ? "You are blocked" : (isReceiverBlocked ? "User is blocked" : "Block user")}
-        </button>
-        <button className="logout" onClick={() => auth.signOut()}>Logout</button>
+        <div className="buttons">
+          <button className="block" onClick={handleBlock}>
+            {/* TODO: Block messages doesn't always show up right! Fix!*/}
+            {isCurrentUserBlocked ? "You are blocked" : (isReceiverBlocked ? "User is blocked" : "Block user")}
+          </button>
+          <button className="logout" onClick={() => auth.signOut()}>Logout</button>
+        </div>
       </div>
     </div>
   )
